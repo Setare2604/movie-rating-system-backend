36aeb7d25f8a1c10b3328a62521cfa94044b865d
Setareh Hojjaty Shargh <fhojjaty00@gmail.com>
2025-12-25 21:17:48 +0330

    feat: add database connection and db health check

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+
+from app.db.database import get_db
 
 app = FastAPI(title="Movie Rating System API")
 
 @app.get("/")
 def root():
     return {"status": "success", "message": "API is running"}
+
+@app.get("/health/db")
+def db_health_check(db: Session = Depends(get_db)):
+    db.execute(text("SELECT 1"))
+    return {"status": "success", "message": "Database connection is OK"}
