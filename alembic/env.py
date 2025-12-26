bcc07b8ee97dacbc3e0eefff6cb3d2e235816606
Setareh Hojjaty Shargh <fhojjaty00@gmail.com>
2025-12-26 14:19:04 +0330

    chore: configure alembic env for dotenv and metadata

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from alembic import context
 import os
 from dotenv import load_dotenv
 from app.db.database import Base
+import app.models  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
